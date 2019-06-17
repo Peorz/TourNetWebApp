d1c4cb60db95eba08e9b565580e37dd90eeee9c7
peorz <760336282@qq.com>
2019-06-17 09:45:13 +0800

    ..

---
+++ TourWebApp/server/mode/FileInfo.cs
@@ -0,0 +1,13 @@
+﻿using NetDB.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourWebApp.server.mode
+{
+    public class FileInfo : ORMSupport
+    {
+        private String _FileName;
+    }
+}
